f79c2ccf5149e34bb74d819417f470b62e3470d0
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-07-24 17:33:31 +0000

    Updated nodegroups config config
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- eksctl/create_cluster.sh
+++ eksctl/create_cluster.sh
@@ -5,8 +5,9 @@ then
   exit 1
 fi
 #"${1}eksctl.sh" create cluster --name="dev-analytics-kube-${1}" --nodes=3 --nodegroup-name="dev-analytics-ng-devstats-${1}" --node-labels "lfda=devstats,node=devstats" --node-type=m5.2xlarge --kubeconfig="/root/.kube/kubeconfig_${1}" --tags "env=${1}" --version=1.13 --node-ami=auto --ssh-access
+# note: only t3.large has SSD only storage, all others have NVMe: https://aws.amazon.com/ec2/instance-types/
 "${1}eksctl.sh" create cluster --name="dev-analytics-kube-${1}" --kubeconfig="/root/.kube/kubeconfig_${1}" --tags "env=${1}" --version=1.13 --ssh-access
+"${1}eksctl.sh" create nodegroup --cluster="dev-analytics-kube-${1}" --nodes=3 --name="dev-analytics-ng-devstats-${1}" --node-type=i3.2xlarge --node-labels "lfda=devstats,node=devstats" --version=1.13 --node-ami=auto --ssh-access
 "${1}eksctl.sh" create nodegroup --cluster="dev-analytics-kube-${1}" --nodes=5 --name="dev-analytics-ng-elastic-${1}" --node-type=i3.large --node-labels "lfda=elastic,node=elastic" --version=1.13 --node-ami=auto --ssh-access
 "${1}eksctl.sh" create nodegroup --cluster="dev-analytics-kube-${1}" --nodes=2 --name="dev-analytics-ng-grimoire-${1}" --node-type=m5d.2xlarge --node-labels "lfda=grimoire,node=grimoire" --version=1.13 --node-ami=auto --ssh-access
 "${1}eksctl.sh" create nodegroup --cluster="dev-analytics-kube-${1}" --nodes=2 --name="dev-analytics-ng-app-${1}" --node-type=t3.large --node-labels "lfda=app,node=app" --version=1.13 --node-ami=auto --ssh-access
-"${1}eksctl.sh" create nodegroup --cluster="dev-analytics-kube-${1}" --nodes=3 --name="dev-analytics-ng-devstats-${1}" --node-type=m5.2xlarge --node-labels "lfda=devstats,node=devstats" --version=1.13 --node-ami=auto --ssh-access
