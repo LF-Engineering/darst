518a83e5530cbd9666d95cb702dad437bb90a1ea
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-07-24 14:53:03 +0000

    Setup local directory on each lfda=elastic node
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- local-storage/setup.sh
+++ local-storage/setup.sh
@@ -5,3 +5,8 @@ then
   exit 1
 fi
 "${1}k.sh" apply -f local-storage/storage-class.yaml
+for ip in `"${1}k.sh" get nodes -l lfda=elastic -o wide | grep 'ip-' | awk '{print $7}'`
+do
+  echo "IP: $ip"
+  ssh "ec2-user@${ip}" 'sudo mkdir /data; sudo chmod ugo+rwx /data'
+done
