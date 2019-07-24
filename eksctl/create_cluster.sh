ca34ed0ceb6381710552cbf681057a9aee2995bc
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-07-24 07:34:26 +0000

    New cluster approach
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- eksctl/create_cluster.sh
+++ eksctl/create_cluster.sh
@@ -4,5 +4,5 @@ then
   echo "$0: you need to specify env: test, dev, stg, prod"
   exit 1
 fi
-#"${1}eksctl.sh" create cluster --name="dev-analytics-kube-${1}" --nodes=12 --kubeconfig="/root/.kube/kubeconfig_${1}" --tags node=devstats --node-volume-type=gp2 --node-volume-size=20 --version=1.13 --nodegroup-name="dev-analytics-ng-${1}" --node-type=m5.2xlarge --node-ami=auto --ssh-access
-"${1}eksctl.sh" create cluster --name="dev-analytics-kube-${1}" --nodes=12 --kubeconfig="/root/.kube/kubeconfig_${1}" --tags node=devstats --node-volume-type=gp2 --node-volume-size=20 --version=1.13 --nodegroup-name="dev-analytics-ng-${1}" --node-type=m5.xlarge --node-ami=auto --ssh-access
+"${1}eksctl.sh" create cluster --name="dev-analytics-kube-${1}" --nodes=4 --nodegroup-name="dev-analytics-ng-devstats-${1}" --node-labels "lfda=devstats,node=devstats" --node-type=m5.2xlarge --kubeconfig="/root/.kube/kubeconfig_${1}" --tags "env=${1}" --node-volume-type=gp2 --node-volume-size=20 --version=1.13 --node-ami=auto --ssh-access
+"${1}eksctl.sh" create nodegroup --cluster="dev-analytics-kube-${1}" --nodes=5 --name="dev-analytics-ng-elastic-${1}" --node-type=i3.large --node-labels "lfda=elastic,node=elastic" --node-volume-type=gp2 --node-volume-size=20 --version=1.13 --node-ami=auto --ssh-access
