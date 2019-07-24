c537918297bac2667732a0698236966829ca8649
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-07-24 14:00:34 +0000

    Fix typo
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- es/delete.sh
+++ es/delete.sh
@@ -5,6 +5,6 @@ then
   exit 1
 fi
 "${1}k.sh" config set-context --current --namespace=dev-analytics-elasticsearch
-"${1}h.sh" delete pv es-data-0 es-data-1 es-data-2 es-data-3 es-data-4
+"${1}k.sh" delete pv es-data-0 es-data-1 es-data-2 es-data-3 es-data-4
 "${1}h.sh" delete dev-analytics-elasticsearch
 "${1}k.sh" config set-context --current --namespace=default
