90581010062946c8c090b726a5dd371e84912fd9
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-07-24 14:14:23 +0000

    Update delete script
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- es/delete.sh
+++ es/delete.sh
@@ -6,5 +6,6 @@ then
 fi
 "${1}k.sh" config set-context --current --namespace=dev-analytics-elasticsearch
 "${1}h.sh" delete dev-analytics-elasticsearch
+"${1}k.sh" delete pvc elasticsearch-master-elasticsearch-master-0 elasticsearch-master-elasticsearch-master-1 elasticsearch-master-elasticsearch-master-2 elasticsearch-master-elasticsearch-master-3 elasticsearch-master-elasticsearch-master-4
 "${1}k.sh" delete pv es-data-0 es-data-1 es-data-2 es-data-3 es-data-4
 "${1}k.sh" config set-context --current --namespace=default
