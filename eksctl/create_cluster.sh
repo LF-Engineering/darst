07ef497438ef55fe526aa8846912ea8dc03591a2
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-07-24 07:42:51 +0000

    Update node-group config
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- eksctl/create_cluster.sh
+++ eksctl/create_cluster.sh
@@ -6,3 +6,5 @@ then
 fi
 "${1}eksctl.sh" create cluster --name="dev-analytics-kube-${1}" --nodes=4 --nodegroup-name="dev-analytics-ng-devstats-${1}" --node-labels "lfda=devstats,node=devstats" --node-type=m5.2xlarge --kubeconfig="/root/.kube/kubeconfig_${1}" --tags "env=${1}" --node-volume-type=gp2 --node-volume-size=20 --version=1.13 --node-ami=auto --ssh-access
 "${1}eksctl.sh" create nodegroup --cluster="dev-analytics-kube-${1}" --nodes=5 --name="dev-analytics-ng-elastic-${1}" --node-type=i3.large --node-labels "lfda=elastic,node=elastic" --node-volume-type=gp2 --node-volume-size=20 --version=1.13 --node-ami=auto --ssh-access
+"${1}eksctl.sh" create nodegroup --cluster="dev-analytics-kube-${1}" --nodes=2 --name="dev-analytics-ng-grimoire-${1}" --node-type=m5d.2xlarge --node-labels "lfda=grimoire,node=grimoire" --node-volume-type=gp2 --node-volume-size=20 --version=1.13 --node-ami=auto --ssh-access
+"${1}eksctl.sh" create nodegroup --cluster="dev-analytics-kube-${1}" --nodes=2 --name="dev-analytics-ng-app-${1}" --node-type=t3.large --node-labels "lfda=app,node=app" --node-volume-type=gp2 --node-volume-size=20 --version=1.13 --node-ami=auto --ssh-access
--- eksctl/delete_cluster.sh
+++ eksctl/delete_cluster.sh
@@ -4,5 +4,8 @@ then
   echo "$0: you need to specify env: test, dev, stg, prod"
   exit 1
 fi
-"${1}eksctl.sh" delete nodegroup --name="dev-analytics-ng-${1}" --cluster "dev-analytics-kube-${1}"
+"${1}eksctl.sh" delete nodegroup --name="dev-analytics-ng-devstats-${1}" --cluster "dev-analytics-kube-${1}"
+"${1}eksctl.sh" delete nodegroup --name="dev-analytics-ng-elastic-${1}" --cluster "dev-analytics-kube-${1}"
+"${1}eksctl.sh" delete nodegroup --name="dev-analytics-ng-grimoire-${1}" --cluster "dev-analytics-kube-${1}"
+"${1}eksctl.sh" delete nodegroup --name="dev-analytics-ng-app-${1}" --cluster "dev-analytics-kube-${1}"
 "${1}eksctl.sh" delete cluster --name="dev-analytics-kube-${1}"
