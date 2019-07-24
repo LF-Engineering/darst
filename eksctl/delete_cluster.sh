e3ae092ad803d06f8a8a9dcb3383611920c8ceb8
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-07-24 17:21:25 +0000

    separate node-groups from cluster completely
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- eksctl/delete_cluster.sh
+++ eksctl/delete_cluster.sh
@@ -8,4 +8,5 @@ fi
 "${1}eksctl.sh" delete nodegroup --name="dev-analytics-ng-elastic-${1}" --cluster "dev-analytics-kube-${1}"
 "${1}eksctl.sh" delete nodegroup --name="dev-analytics-ng-grimoire-${1}" --cluster "dev-analytics-kube-${1}"
 "${1}eksctl.sh" delete nodegroup --name="dev-analytics-ng-app-${1}" --cluster "dev-analytics-kube-${1}"
+"${1}eksctl.sh" delete nodegroup --name="dev-analytics-ng-devstats-${1}" --cluster "dev-analytics-kube-${1}"
 "${1}eksctl.sh" delete cluster --name="dev-analytics-kube-${1}"
