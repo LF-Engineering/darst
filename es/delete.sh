4177e1f8cf912a3e8239830ee694a4632b0458cd
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-07-25 04:21:03 +0000

    Fix missing delete
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- es/delete.sh
+++ es/delete.sh
@@ -5,6 +5,7 @@ then
   exit 1
 fi
 change_namespace.sh $1 dev-analytics-elasticsearch
+"${1}h.sh" delete dev-analytics-elasticsearch
 "${1}k.sh" delete pvc elasticsearch-master-elasticsearch-master-0 elasticsearch-master-elasticsearch-master-1 elasticsearch-master-elasticsearch-master-2 elasticsearch-master-elasticsearch-master-3 elasticsearch-master-elasticsearch-master-4
 "${1}k.sh" delete pv es-data-0 es-data-1 es-data-2 es-data-3 es-data-4
 change_namespace.sh $1 default
