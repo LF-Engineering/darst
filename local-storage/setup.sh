8cdfb0447178cce2396c57242c82c2ef4fd2dd1f
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-07-25 07:45:22 +0000

    Update script 3
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- local-storage/setup.sh
+++ local-storage/setup.sh
@@ -9,23 +9,20 @@ fi
 # Mount NVMes in elastic node-group
 for ip in `"${1}k.sh" get nodes -l lfda=elastic -o wide | grep 'ip-' | awk '{print $7}'`
 do
-  echo "IP: $ip"
-  # Eventually mkfs.xfs -f (to force create bFS even if FS already exists)
-  ssh "ec2-user@${ip}" 'sudo mkfs -t xfs /dev/nvme0n1; sudo mkdir /var/openebs; sudo chmod ugo+rwx /var/openebs; echo "`sudo blkid /dev/nvme0n1 | awk '"'"'{print $2}'"'"'` /var/openebs xfs defaults 0 0" | sudo tee -a /etc/fstab; sudo mount /openebs'
+  echo "elastic node group ip: $ip"
+  ssh "ec2-user@${ip}" 'sudo mkfs.xfs -f /dev/nvme0n1; sudo mkdir /var/openebs; sudo chmod ugo+rwx /var/openebs; echo "`sudo blkid /dev/nvme0n1 | awk '"'"'{print $2}'"'"'` /var/openebs xfs defaults 0 0" | sudo tee -a /etc/fstab; sudo mount /openebs'
 done
 
 # Mount NVMes in devstats node-group
 for ip in `"${1}k.sh" get nodes -l lfda=devstats -o wide | grep 'ip-' | awk '{print $7}'`
 do
-  echo "IP: $ip"
-  # Eventually mkfs.xfs -f (to force create bFS even if FS already exists)
-  ssh "ec2-user@${ip}" 'sudo mkfs -t xfs /dev/nvme0n1; sudo mkdir /var/openebs; sudo chmod ugo+rwx /var/openebs; echo "`sudo blkid /dev/nvme0n1 | awk '"'"'{print $2}'"'"'` /var/openebs xfs defaults 0 0" | sudo tee -a /etc/fstab; sudo mount /var/openebs'
+  echo "devstats node group ip: $ip"
+  ssh "ec2-user@${ip}" 'sudo mkfs.xfs -f /dev/nvme0n1; sudo mkdir /var/openebs; sudo chmod ugo+rwx /var/openebs; echo "`sudo blkid /dev/nvme0n1 | awk '"'"'{print $2}'"'"'` /var/openebs xfs defaults 0 0" | sudo tee -a /etc/fstab; sudo mount /var/openebs'
 done
 
 # Mount NVMes in grimoire node-group
 for ip in `"${1}k.sh" get nodes -l lfda=grimoire -o wide | grep 'ip-' | awk '{print $7}'`
 do
-  echo "IP: $ip"
-  # Eventually mkfs.xfs -f (to force create bFS even if FS already exists)
-  ssh "ec2-user@${ip}" 'sudo mkfs -t xfs /dev/nvme1n1; sudo mkdir /var/openebs; sudo chmod ugo+rwx /var/openebs; echo "`sudo blkid /dev/nvme1n1 | awk '"'"'{print $2}'"'"'` /var/openebs xfs defaults 0 0" | sudo tee -a /etc/fstab; sudo mount /var/openebs'
+  echo "grimoire node group ip: $ip"
+  ssh "ec2-user@${ip}" 'sudo mkfs.xfs -f /dev/nvme1n1; sudo mkdir /var/openebs; sudo chmod ugo+rwx /var/openebs; echo "`sudo blkid /dev/nvme1n1 | awk '"'"'{print $2}'"'"'` /var/openebs xfs defaults 0 0" | sudo tee -a /etc/fstab; sudo mount /var/openebs'
 done
