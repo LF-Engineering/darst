809404acf8482295d79aae1c0cf6c49d3c6ca118
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-07-25 09:00:48 +0000

    Fix request
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- openebs/setup.sh
+++ openebs/setup.sh
@@ -6,4 +6,4 @@ then
   exit 1
 fi
 "${1}k.sh" apply -f https://openebs.github.io/charts/openebs-operator-1.0.0.yaml
-"${1}h.sh" install local-storage-nfs stable/nfs-server-provisioner --set=persistence.enabled=true,persistence.storageClass=openebs-hostpath,persistence.size=200i,storageClass.name=nfs-openebs-localstorage
+"${1}h.sh" install local-storage-nfs stable/nfs-server-provisioner --set=persistence.enabled=true,persistence.storageClass=openebs-hostpath,persistence.size=160Gi,storageClass.name=nfs-openebs-localstorage
