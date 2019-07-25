9620f1243212372e70b3e7852aaa7fe7720da18f
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-07-25 04:28:37 +0000

    XFS creation update
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- local-storage/setup.sh
+++ local-storage/setup.sh
@@ -8,5 +8,6 @@ fi
 for ip in `"${1}k.sh" get nodes -l lfda=elastic -o wide | grep 'ip-' | awk '{print $7}'`
 do
   echo "IP: $ip"
-  ssh "ec2-user@${ip}" 'sudo mkfs.xfs -f /dev/nvme0n1; sudo mkdir /data; sudo chmod ugo+rwx /data; echo "`sudo blkid /dev/nvme0n1 | awk '"'"'{print $2}'"'"'` /data xfs defaults 0 0" | sudo tee -a /etc/fstab; sudo mount /data'
+  # Eventually mkfs.xfs -f (to force create bFS even if FS already exists)
+  ssh "ec2-user@${ip}" 'sudo mkfs -t xfs /dev/nvme0n1; sudo mkdir /data; sudo chmod ugo+rwx /data; echo "`sudo blkid /dev/nvme0n1 | awk '"'"'{print $2}'"'"'` /data xfs defaults 0 0" | sudo tee -a /etc/fstab; sudo mount /data'
 done
