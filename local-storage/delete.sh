212f53254e14a69e8200069d0dbfdc93561de919
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-07-25 07:37:30 +0000

    Update script
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- local-storage/delete.sh
+++ local-storage/delete.sh
@@ -9,21 +9,21 @@ fi
 for ip in `"${1}k.sh" get nodes -l lfda=elastic -o wide | grep 'ip-' | awk '{print $7}'`
 do
   echo "elastic node group ip: $ip"
-  ssh "ec2-user@${ip}" 'sudo umount -f /var/openebs; sudo rm -rf /var/openebs; sudo vi -c "g/\/var\/openebs/d" -c "wq" "a"'
+  ssh "ec2-user@${ip}" 'sudo umount -f /var/openebs; sudo rm -rf /var/openebs; sudo vi -c "g/\/var\/openebs/d" -c "wq" "/etc/fstab"'
 done
 
 # devstats node-group
 for ip in `"${1}k.sh" get nodes -l lfda=devstats -o wide | grep 'ip-' | awk '{print $7}'`
 do
   echo "devstats node group ip: $ip"
-  ssh "ec2-user@${ip}" 'sudo umount -f /var/openebs; sudo rm -rf /var/openebs; sudo vi -c "g/\/var\/openebs/d" -c "wq" "a"'
+  ssh "ec2-user@${ip}" 'sudo umount -f /var/openebs; sudo rm -rf /var/openebs; sudo vi -c "g/\/var\/openebs/d" -c "wq" "/etc/fstab"'
 done
 
 # grimoire node-group
 for ip in `"${1}k.sh" get nodes -l lfda=grimoire -o wide | grep 'ip-' | awk '{print $7}'`
 do
   echo "grimoire node group ip: $ip"
-  ssh "ec2-user@${ip}" 'sudo umount -f /var/openebs; sudo rm -rf /var/openebs; sudo vi -c "g/\/var\/openebs/d" -c "wq" "a"'
+  ssh "ec2-user@${ip}" 'sudo umount -f /var/openebs; sudo rm -rf /var/openebs; sudo vi -c "g/\/var\/openebs/d" -c "wq" "/etc/fstab"'
 done
 
 "${1}k.sh" delete -f local-storage/storage-class.yaml
