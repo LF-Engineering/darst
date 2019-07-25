73f2dccad2082fceb3f75258c2abeee1cda8094e
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-07-25 07:22:08 +0000

    delete.sh for OpenEBS and nfs provisioner
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ openebs/delete.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#!/bin/bash
+if  [ -z "$1" ]
+then
+  echo "$0: you need to specify env: test, dev, stg, prod"
+  exit 1
+fi
+"${1}h.sh" delete local-storage-nfs
+"${1}k.sh" delete -f https://openebs.github.io/charts/openebs-operator-1.0.0.yaml
