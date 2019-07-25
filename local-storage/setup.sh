cf60f18ffd58ea4f98c7675b9c269d87ab3ac692
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-07-25 07:48:25 +0000

    Update scripts
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- local-storage/setup.sh
+++ local-storage/setup.sh
@@ -10,7 +10,7 @@ fi
 for ip in `"${1}k.sh" get nodes -l lfda=elastic -o wide | grep 'ip-' | awk '{print $7}'`
 do
   echo "elastic node group ip: $ip"
-  ssh "ec2-user@${ip}" 'sudo mkfs.xfs -f /dev/nvme0n1; sudo mkdir /var/openebs; sudo chmod ugo+rwx /var/openebs; echo "`sudo blkid /dev/nvme0n1 | awk '"'"'{print $2}'"'"'` /var/openebs xfs defaults 0 0" | sudo tee -a /etc/fstab; sudo mount /openebs'
+  ssh "ec2-user@${ip}" 'sudo mkfs.xfs -f /dev/nvme0n1; sudo mkdir /var/openebs; sudo chmod ugo+rwx /var/openebs; echo "`sudo blkid /dev/nvme0n1 | awk '"'"'{print $2}'"'"'` /var/openebs xfs defaults 0 0" | sudo tee -a /etc/fstab; sudo mount /var/openebs'
 done
 
 # Mount NVMes in devstats node-group
