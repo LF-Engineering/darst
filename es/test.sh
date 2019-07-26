d1b8d3cc977328aadfa9040b975b2a1962913c6d
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-07-26 11:15:39 +0000

    Update scripts
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- es/test.sh
+++ es/test.sh
@@ -4,4 +4,5 @@ then
   echo "$0: you need to specify env: test, dev, stg, prod"
   exit 1
 fi
-"${1}k.sh" run curl-test-es -i --tty --restart=Never --rm --image="radial/busyboxplus:curl" --command "/usr/bin/curl" -- 'elasticsearch-master.dev-analytics-elasticsearch:9200'
+"${1}k.sh" run --generator=run-pod/v1 curl-test-es -i --tty --restart=Never --rm --image="radial/busyboxplus:curl" --command "/usr/bin/curl" -- 'elasticsearch-master.dev-analytics-elasticsearch:9200'
+"${1}k.sh" delete pod curl-test-es
