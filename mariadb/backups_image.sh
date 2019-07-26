648e35da6756d35b8785750a54000e3862f527cf
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-07-26 13:45:31 +0000

    Fix bug in push script
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- mariadb/backups_image.sh
+++ mariadb/backups_image.sh
@@ -8,12 +8,12 @@ fi
 
 docker build -f ./mariadb/Dockerfile.mariadb.backups -t "${DOCKER_USER}/mariadb-backups" . || exit 2
 
-if [ ! -z "$SKIP_PUSH" ]
+if [ -z "$SKIP_PUSH" ]
 then
   docker push "${DOCKER_USER}/mariadb-backups" || exit 2
 fi
 
-if [ ! -z "$SKIP_RM" ]
+if [ -z "$SKIP_RM" ]
 then
   docker image rm -f "${DOCKER_USER}/mariadb-backups"
 fi
