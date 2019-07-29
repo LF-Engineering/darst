de9e1709f6d5be930e17b8ad541441d9f13d4d25
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-07-29 08:20:41 +0000

    Fix typo 3
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- redis/setup.sh
+++ redis/setup.sh
@@ -20,5 +20,5 @@ vim --not-a-term -c "%s/PASS/${pass}/g" -c 'wq!' "$fn"
 "${1}k.sh" create -f redis/namespace.yaml
 change_namespace.sh $1 redis
 "${1}k.sh" -n redis create -f "$fn"
-"${1}h.sh" -n redis redis stable/redis --set "existingSecret=redis-secret,master.nodeSelector.lfda=grimoire,master.persistence.storageClass=openebs-hostpath,master.persistence.size=4Gi,master.resources.requests.cpu=250m,master.resources.requests.memory=256Mi,master.resources.limits.cpu=2000m,master.resources.limits.memory=2Gi,slave.nodeSelector.lfda=grimoire,slave.persistence.storageClass=openebs-hostpath,slave.persistence.size=4Gi,slave.resources.requests.cpu=250m,slave.resources.requests.memory=256Mi,slave.resources.limits.cpu=2000m,slave.resources.limits.memory=2Gi"
+"${1}h.sh" -n redis install redis stable/redis --set "existingSecret=redis-secret,master.nodeSelector.lfda=grimoire,master.persistence.storageClass=openebs-hostpath,master.persistence.size=4Gi,master.resources.requests.cpu=250m,master.resources.requests.memory=256Mi,master.resources.limits.cpu=2000m,master.resources.limits.memory=2Gi,slave.nodeSelector.lfda=grimoire,slave.persistence.storageClass=openebs-hostpath,slave.persistence.size=4Gi,slave.resources.requests.cpu=250m,slave.resources.requests.memory=256Mi,slave.resources.limits.cpu=2000m,slave.resources.limits.memory=2Gi"
 change_namespace.sh $1 default
