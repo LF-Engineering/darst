3f42b1dfec6b7e55030303af3f8f72b68bb38341
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-07-29 08:38:08 +0000

    Redis test script
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- redis/test.sh
+++ redis/test.sh
@@ -4,5 +4,5 @@ then
   echo "$0: you need to specify env: test, dev, stg, prod"
   exit 1
 fi
-"${1}k.sh" run --generator=run-pod/v1 curl-test-es -i --tty --restart=Never --rm --image="radial/busyboxplus:curl" --command "/usr/bin/curl" -- 'redis.redis:6379'
+"${1}k.sh" run --generator=run-pod/v1 redis-test -i --tty --restart=Never --rm --image="bitnami/redis" --env="REDISCLI_AUTH=`cat redis/secrets/PASS.${1}.secret`" -- redis-cli -h redis-master.redis ping
 #"${1}k.sh" delete pod curl-test-es
