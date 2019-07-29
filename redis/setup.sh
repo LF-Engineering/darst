b774231b8720d2bed27b0744c25ddbb14aff53e7
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-07-29 08:19:12 +0000

    Fix typo 2
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- redis/setup.sh
+++ redis/setup.sh
@@ -13,12 +13,12 @@ cp redis/secret.yaml "$fn"
 pass=`cat "redis/secrets/PASS.${1}.secret" | base64`
 if [ -z "$pass" ]
 then
-  echo "$0: you need to provide password value in mariadb/secrets/PASS.${1}.secret"
+  echo "$0: you need to provide password value in redis/secrets/PASS.${1}.secret"
   exit 1
 fi
 vim --not-a-term -c "%s/PASS/${pass}/g" -c 'wq!' "$fn"
 "${1}k.sh" create -f redis/namespace.yaml
 change_namespace.sh $1 redis
-"${1}k.sh" -n mariadb create -f "$fn"
+"${1}k.sh" -n redis create -f "$fn"
 "${1}h.sh" -n redis redis stable/redis --set "existingSecret=redis-secret,master.nodeSelector.lfda=grimoire,master.persistence.storageClass=openebs-hostpath,master.persistence.size=4Gi,master.resources.requests.cpu=250m,master.resources.requests.memory=256Mi,master.resources.limits.cpu=2000m,master.resources.limits.memory=2Gi,slave.nodeSelector.lfda=grimoire,slave.persistence.storageClass=openebs-hostpath,slave.persistence.size=4Gi,slave.resources.requests.cpu=250m,slave.resources.requests.memory=256Mi,slave.resources.limits.cpu=2000m,slave.resources.limits.memory=2Gi"
 change_namespace.sh $1 default
