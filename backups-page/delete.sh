e22503d5a4635a6938fcd226265b6de6f44a68fb
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-07-31 12:51:58 +0000

    Attempt to create withing the same namespace
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- backups-page/delete.sh
+++ backups-page/delete.sh
@@ -4,8 +4,10 @@ then
   echo "$0: you need to specify env: test, dev, stg, prod"
   exit 1
 fi
-change_namespace.sh $1 backups
-"${1}h.sh" delete dev-analytics-elasticsearch
+change_namespace.sh $1 devstats
+"${1}k.sh" delete svc backups-page-service
+"${1}k.sh" delete deployment backups-page
+change_namespace.sh $1 mariadb
 "${1}k.sh" delete svc backups-page-service
 "${1}k.sh" delete deployment backups-page
 change_namespace.sh $1 default
--- backups-page/setup.sh
+++ backups-page/setup.sh
@@ -4,7 +4,8 @@ then
   echo "$0: you need to specify env: test, dev, stg, prod"
   exit 1
 fi
-"${1}k.sh" create -f backups-page/namespace.yaml
-change_namespace.sh $1 backups
-"${1}k.sh" create -f backups-page/backups-page.yaml
+change_namespace.sh $1 devstats
+"${1}k.sh" create -f backups-page/backups-page-postgres.yaml
+change_namespace.sh $1 mariadb
+"${1}k.sh" create -f backups-page/backups-page-mariadb.yaml
 change_namespace.sh $1 default
