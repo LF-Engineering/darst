cea812e148b520d150b5c20f827e5b7a00f7ffdc
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-08-01 09:40:05 +0000

    Enchance delete script
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- dev_analytics/delete_db.sh
+++ dev_analytics/delete_db.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+psql -h$PG_HOST -U$PG_USER postgres -c "select pg_terminate_backend(pid) from pg_stat_activity where datname = 'dev_analytics'"
 dropdb -h$PG_HOST -U$PG_USER dev_analytics
