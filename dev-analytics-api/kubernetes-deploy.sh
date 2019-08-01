1d0de936064b14db151c774f39557f03617a3005
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-08-01 12:36:21 +0000

    Set TASK_ID
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- dev-analytics-api/kubernetes-deploy.sh
+++ dev-analytics-api/kubernetes-deploy.sh
@@ -7,4 +7,4 @@ fi
 . env.sh "$1" || exit 1
 cd ~/dev/dev-analytics-api/.circleci/deployments/$API_DIR || exit 3
 context="`cat ~/.kube/kubeconfig_$KUBCONFIGE_SUFF | grep '^\- name: ' | awk '{print $3}'`"
-kubernetes-deploy "dev-analytics-api-$ENV_NS" "$context" --template-dir=.
+TASK_ID=`date +'%s%N'` kubernetes-deploy "dev-analytics-api-$ENV_NS" "$context" --template-dir=.
