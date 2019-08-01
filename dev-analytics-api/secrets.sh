a08be24a5d700dbc441035bc7a11bbc32fd087d5
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-08-01 11:59:56 +0000

    Fix
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- dev-analytics-api/secrets.sh
+++ dev-analytics-api/secrets.sh
@@ -10,7 +10,7 @@ seed_env_short=dev
 seed_env_long=develop
 env_short=$1
 function finish {
-  change_namespace.sh $env_short default
+  change_namespace.sh $ENV_NS_short default
 }
 trap finish EXIT
 cd ~/dev/dev-analytics-api || exit 1
@@ -21,17 +21,17 @@ export PUBLIC_KEY_VALUE=$(cat .circleci/deployments/$API_DIR/secrets.ejson | jq 
 export PRIVATE_KEY_VALUE=$(${seed_env_short}k.sh get secret ejson-keys --namespace=dev-analytics-api-$seed_env_long -o json | jq -rc '.data.'$PUBLIC_KEY_VALUE | base64 --decode)
 "${1}k.sh" create secret generic ejson-keys --from-literal=$PUBLIC_KEY_VALUE=$PRIVATE_KEY_VALUE --namespace=dev-analytics-api-$ENV_NS
 cd .circleci/deployments || exit 2
-./update-secret.sh $env DATABASE_HOST "`cat ~/dev/da-patroni/da-patroni/secrets/PG_HOST.secret`"
-./update-secret.sh $env DATABASE_PASSWORD "`cat ~/dev/da-patroni/da-patroni/secrets/PG_PASS.$1.secret`"
-./update-secret.sh $env DATABASE_USERNAME "`cat ~/dev/da-patroni/da-patroni/secrets/PG_USER.secret`"
-./update-secret.sh $env RAILS_ENV $ENV_NS
-./update-secret.sh $env REDIS_URL_ROOT "redis://redis.redis"
-./update-secret.sh $env REDIS_URL "`cat ~/dev/darst/redis/secrets/URL.secret`"
-./update-secret.sh $env DEVSTATS_DB_HOST "`cat ~/dev/da-patroni/da-patroni/secrets/PG_HOST.secret`"
-./update-secret.sh $env SORTINGHAT_HOST "`cat ~/dev/darst/mariadb/secrets/HOST.secret`"
+./update-secret.sh $ENV_NS DATABASE_HOST "`cat ~/dev/da-patroni/da-patroni/secrets/PG_HOST.secret`"
+./update-secret.sh $ENV_NS DATABASE_PASSWORD "`cat ~/dev/da-patroni/da-patroni/secrets/PG_PASS.$1.secret`"
+./update-secret.sh $ENV_NS DATABASE_USERNAME "`cat ~/dev/da-patroni/da-patroni/secrets/PG_USER.secret`"
+./update-secret.sh $ENV_NS RAILS_ENV $ENV_NS
+./update-secret.sh $ENV_NS REDIS_URL_ROOT "redis://redis.redis"
+./update-secret.sh $ENV_NS REDIS_URL "`cat ~/dev/darst/redis/secrets/URL.secret`"
+./update-secret.sh $ENV_NS DEVSTATS_DB_HOST "`cat ~/dev/da-patroni/da-patroni/secrets/PG_HOST.secret`"
+./update-secret.sh $ENV_NS SORTINGHAT_HOST "`cat ~/dev/darst/mariadb/secrets/HOST.secret`"
 # FIXME: we should have those keys
-./update-secret.sh $env SORTINGHAT_USER "`cat ~/dev/darst/mariadb/secrets/USER.secret`"
-./update-secret.sh $env SORTINGHAT_PASSWORD "`cat ~/dev/darst/mariadb/secrets/PASS.$1.secret`"
+./update-secret.sh $ENV_NS SORTINGHAT_USER "`cat ~/dev/darst/mariadb/secrets/USER.secret`"
+./update-secret.sh $ENV_NS SORTINGHAT_PASSWORD "`cat ~/dev/darst/mariadb/secrets/PASS.$1.secret`"
 # FIXME: how about those keys?
-# ./update-secret.sh $env RAILS_MASTER_KEY '?' (its not needed probably), how about SalesForce keys?
-./update-secret.sh $env
+# ./update-secret.sh $ENV_NS RAILS_MASTER_KEY '?' (its not needed probably), how about SalesForce keys?
+./update-secret.sh $ENV_NS
