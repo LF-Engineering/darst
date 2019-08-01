dec34c183b2afc7754ada316ef7b37a108f6eff6
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-08-01 12:04:05 +0000

    Fix script
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- dev-analytics-api/setup.sh
+++ dev-analytics-api/setup.sh
@@ -4,4 +4,5 @@ then
   echo "$0: you need to specify env: test, dev, stg, prod"
   exit 1
 fi
-./dev-analytics-api/secrets.sh "$1" && ./dev-analytics-api/kubernetes-deploy.sh "$1"
+./dev-analytics-api/secrets.sh "$1" || exit 2
+./dev-analytics-api/kubernetes-deploy.sh "$1"
