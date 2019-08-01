bd09b6eecaf6b8d77e8d46db9320583eb03f4227
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-08-01 16:48:45 +0000

    Simpler get context
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- dev-analytics-api/kubernetes-deploy.sh
+++ dev-analytics-api/kubernetes-deploy.sh
@@ -20,5 +20,5 @@ mkdir "$dd"
 cp ~/dev/dev-analytics-api/.circleci/deployments/$API_DIR/* "$dd" || exit 3
 vim --not-a-term -c "%s/image: .*/image: $DOCKER_USER\/dev-analytics-api/g" -c 'wq!' "${dd}/api.deployment.yml.erb"
 vim --not-a-term -c "%s/image: .*/image: $DOCKER_USER\/dev-analytics-api/g" -c 'wq!' "${dd}/migrate.yml.erb"
-context="`cat ~/.kube/kubeconfig_$KUBCONFIGE_SUFF | grep '^\- name: ' | awk '{print $3}'`"
+context="`${1}k.sh config current-context`"
 TASK_ID=`date +'%s%N'` kubernetes-deploy "dev-analytics-api-$ENV_NS" "$context" --template-dir="$dd"
