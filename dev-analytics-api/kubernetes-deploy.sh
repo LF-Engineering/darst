6ecda79d4669b98ddba57af086308c81b6fa2685
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-08-01 16:19:01 +0000

    Do custom deployment using vim regexp
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- dev-analytics-api/kubernetes-deploy.sh
+++ dev-analytics-api/kubernetes-deploy.sh
@@ -5,6 +5,15 @@ then
   exit 1
 fi
 . env.sh "$1" || exit 1
-cd ~/dev/dev-analytics-api/.circleci/deployments/$API_DIR || exit 3
+dd=/tmp/k8sdeploy
+function finish {
+  rm -rf "$dd"
+}
+trap finish EXIT
+rm -rf "$dd"
+mkdir "$dd"
+cp ~/dev/dev-analytics-api/.circleci/deployments/$API_DIR/* "$dd" || exit 3
+vim --not-a-term -c '%s/image: .*/image: lukaszgryglicki\/dev-analytics-api/g' -c 'wq!' "${dd}/api.deployment.yml.erb"
+vim --not-a-term -c '%s/image: .*/image: lukaszgryglicki\/dev-analytics-api/g' -c 'wq!' "${dd}/migrate.yml.erb"
 context="`cat ~/.kube/kubeconfig_$KUBCONFIGE_SUFF | grep '^\- name: ' | awk '{print $3}'`"
-TASK_ID=`date +'%s%N'` kubernetes-deploy "dev-analytics-api-$ENV_NS" "$context" --template-dir=.
+TASK_ID=`date +'%s%N'` kubernetes-deploy "dev-analytics-api-$ENV_NS" "$context" --template-dir="$dd"
