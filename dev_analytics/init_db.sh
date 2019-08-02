bca83fb8d06bc33f5079e9ce48f5eb3d9f6d8844
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-08-02 05:52:02 +0000

    PgCrypto extension needed per database
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- dev_analytics/init_db.sh
+++ dev_analytics/init_db.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 dropdb -h$PG_HOST -U$PG_USER dev_analytics
 psql -h$PG_HOST -U$PG_USER < init.sql
+psql -h$PG_HOST -U$PG_USER dev_analytics -c 'create extension if not exists pgcrypto schema public'
 if [ "$1" = "test" ]
 then
   dropdb -h$PG_HOST -U$PG_USER "dev_analytics_$1"
   psql -h$PG_HOST -U$PG_USER < "init_$1.sql"
+  psql -h$PG_HOST -U$PG_USER "dev_analytics_$1" -c 'create extension if not exists pgcrypto schema public'
 fi
