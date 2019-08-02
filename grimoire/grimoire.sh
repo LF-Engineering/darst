25dea9f7dda2becc6e5f65d6f0bafc516c19161f
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-08-02 14:36:24 +0000

    Debug more
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- grimoire/grimoire.sh
+++ grimoire/grimoire.sh
@@ -98,10 +98,11 @@ vim --not-a-term -c "%s/NAME/${name}/g" -c 'wq!' "$fn"
 change_namespace.sh $1 "$name"
 if [ "$op" = "install" ]
 then
-  "${1}h.sh" install "$name" ./grimoire/grimoire-chart $FLAGS --set api.url="$api_url",projectSlug="$slug", image.repository="$repository",image.account="$account",image.tag="$tag",identity.image.repository="$identity_repository",identity.image.account="$account",identity.image.tag="$identity_tag",identity.db.name="$identity_database"
+  echo "${1}h.sh" install "$name" ./grimoire/grimoire-chart $FLAGS -n $name --set api.url="$api_url",projectSlug="$slug", image.repository="$repository",image.account="$account",image.tag="$tag",identity.image.repository="$identity_repository",identity.image.account="$account",identity.image.tag="$identity_tag",identity.db.name="$identity_database"
+  "${1}h.sh" install "$name" ./grimoire/grimoire-chart $FLAGS -n $name --set api.url="$api_url",projectSlug="$slug", image.repository="$repository",image.account="$account",image.tag="$tag",identity.image.repository="$identity_repository",identity.image.account="$account",identity.image.tag="$identity_tag",identity.db.name="$identity_database"
 elif [ "$op" = "upgrade" ]
 then
-  "${1}h.sh" upgrade "$name" ./grimoire/grimoire-chart $FLAGS --reuse-values --set api.url="$api_url",projectSlug="$slug", image.repository="$repository",image.account="$account",image.tag="$tag",identity.image.repository="$identity_repository",identity.image.account="$account",identity.image.tag="$identity_tag",identity.db.name="$identity_database"
+  "${1}h.sh" upgrade "$name" ./grimoire/grimoire-chart $FLAGS -n $name --reuse-values --set api.url="$api_url",projectSlug="$slug", image.repository="$repository",image.account="$account",image.tag="$tag",identity.image.repository="$identity_repository",identity.image.account="$account",identity.image.tag="$identity_tag",identity.db.name="$identity_database"
 else
   echo "$0: unknown operation: $op"
   exit 9
