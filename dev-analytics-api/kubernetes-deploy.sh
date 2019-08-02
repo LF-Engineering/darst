a88491c8a58d80f5cb9ffce39e205daf5d710c35
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-08-02 10:48:19 +0000

    more time for db:seed
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- dev-analytics-api/kubernetes-deploy.sh
+++ dev-analytics-api/kubernetes-deploy.sh
@@ -19,6 +19,6 @@ rm -rf "$dd"
 mkdir "$dd"
 cp ~/dev/dev-analytics-api/.circleci/deployments/$API_DIR/* "$dd" || exit 3
 vim --not-a-term -c "%s/image: .*/image: $DOCKER_USER\/dev-analytics-api/g" -c 'wq!' "${dd}/api.deployment.yml.erb"
-vim --not-a-term -c "%s/image: .*/image: $DOCKER_USER\/dev-analytics-api/g" -c '%s/"bundle", "exec", "rails", "db:migrate"/"bundle", "exec", "rails", "db:migrate", "db:seed"/g' -c '%s/120/300/g' -c 'wq!' "${dd}/migrate.yml.erb"
+vim --not-a-term -c "%s/image: .*/image: $DOCKER_USER\/dev-analytics-api/g" -c '%s/"bundle", "exec", "rails", "db:migrate"/"bundle", "exec", "rails", "db:migrate", "db:seed"/g' -c '%s/120/1800/g' -c 'wq!' "${dd}/migrate.yml.erb"
 context="`${1}k.sh config current-context`"
 TASK_ID=`date +'%s%N'` kubernetes-deploy "dev-analytics-api-$ENV_NS" "$context" --template-dir="$dd"
