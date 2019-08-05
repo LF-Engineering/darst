a00d7a3ba1fb2959f1a6378469e4e24d7d6e3720
lukaszgryglicki <lukaszgryglicki@o2.pl>
2019-08-05 12:36:58 +0000

    Pass all SH related stuff
    Signed-off-by: lukaszgryglicki <lukaszgryglicki@o2.pl>

--- dev-analytics-api/kubernetes-deploy.sh
+++ dev-analytics-api/kubernetes-deploy.sh
@@ -18,11 +18,12 @@ trap finish EXIT
 rm -rf "$dd"
 mkdir "$dd"
 cp ~/dev/dev-analytics-api/.circleci/deployments/$API_DIR/* "$dd" || exit 3
-vim --not-a-term -c "%s/image: .*/image: $DOCKER_USER\/dev-analytics-api/g" -c '%s/"bundle", "exec", "rails", "s", "-b", "0\.0\.0\.0"/"\/bin\/sh", "-c", "bundle exec rails db:seed \&\& bundle exec rails s -b 0\.0\.0\.0"/g' -c 'wq!' "${dd}/api.deployment.yml.erb"
+vim --not-a-term -c "%s/image: .*/image: $DOCKER_USER\/dev-analytics-api/g" -c '%s/"bundle", "exec", "rails", "s", "-b", "0\.0\.0\.0"/"\/bin\/sh", "-c", "bundle exec rails db:reset \&\& bundle exec rails s -b 0\.0\.0\.0"/g' -c 'wq!' "${dd}/api.deployment.yml.erb"
 vim --not-a-term -c "%s/image: .*/image: $DOCKER_USER\/dev-analytics-api/g" -c 'wq!' "${dd}/migrate.yml.erb"
+cat dev-analytics-api/sortinghat.partial >> "${dd}/api.deployment.yml.erb"
 if [ "$1" = "test" ]
 then
-  cat dev-analytics-api/sortinghat.partial >> "${dd}/api.deployment.yml.erb"
+  cat dev-analytics-api/sortinghat.partial.test >> "${dd}/api.deployment.yml.erb"
 fi
 context="`${1}k.sh config current-context`"
 TASK_ID=`date +'%s%N'` kubernetes-deploy "dev-analytics-api-$ENV_NS" "$context" --template-dir="$dd"
