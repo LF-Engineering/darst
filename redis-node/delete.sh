d817d4ce48d4a3ee140f853d50c5da51104a44d1
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-08-06 12:42:10 +0000

    Fix typo
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- redis-node/delete.sh
+++ redis-node/delete.sh
@@ -5,10 +5,10 @@ then
   exit 1
 fi
 
-# remove redis settings and restore THP to madvice mode
+# remove redis settings and restore THP to madvise mode
 for ip in `testk.sh get nodes -l lfda=grimoire -o wide | grep 'ip-' | awk '{print $7}'`
 do
   echo "grimoire node group ip: $ip"
-  ssh "ec2-user@${ip}" 'sudo vi -e -s -c "g/net\.core\.somaxconn/d" -c "g/vm\.overcommit_memory/d" -c "wq" /etc/rc.local; echo "madvice" | sudo tee /sys/kernel/mm/transparent_hugepage/enabled'
+  ssh "ec2-user@${ip}" 'sudo vi -e -s -c "g/net\.core\.somaxconn/d" -c "g/vm\.overcommit_memory/d" -c "wq" /etc/rc.local; echo "madvise" | sudo tee /sys/kernel/mm/transparent_hugepage/enabled'
 done
 
