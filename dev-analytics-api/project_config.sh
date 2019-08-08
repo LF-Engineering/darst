35808e1aee4457a120abd55e5cdfb60b7c1fc43e
lukaszgryglicki <lukaszgryglicki@o2.pl>
2019-08-08 07:45:34 +0000

    Support internal or external API server
    Signed-off-by: lukaszgryglicki <lukaszgryglicki@o2.pl>

--- dev-analytics-api/project_config.sh
+++ dev-analytics-api/project_config.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# NO_DNS=1 - use internal API server (raw AWS ELB) instead of external DNS
 if [ -z "$1" ]
 then
   echo "$0: you need to specify env: test, dev, stg, prod"
@@ -9,7 +10,12 @@ then
   echo "$0: you need to specify project as a second arg"
   exit 2
 fi
-api=`"${1}k.sh" -n "dev-analytics-api-$1" get svc | grep dev-analytics-api-lb | head -n 1 | awk '{ print $4 }'`
+. env.sh "$1" || exit 1
+api="https://api.${TF_DIR}.lfanalytics.io"
+if [ ! -z "$NO_DNS" ]
+then
+  api=`"${1}k.sh" -n "dev-analytics-api-$1" get svc | grep dev-analytics-api-lb | head -n 1 | awk '{ print $4 }'`
+fi
 api="${api}/api/internal/grimoire/configuration/$2"
 for type in mordred environment aliases projects credentials
 do
