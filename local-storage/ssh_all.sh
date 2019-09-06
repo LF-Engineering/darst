0e9a7d407b33ac1351f4f74aa171aaf8fe9e9604
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-09-06 05:58:22 +0000

    Added SSH all script 2
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- local-storage/ssh_all.sh
+++ local-storage/ssh_all.sh
@@ -6,23 +6,30 @@ then
 fi
 "${1}k.sh" apply -f local-storage/storage-class.yaml
 
-# Mount NVMes in elastic node-group
+# elastic node-group
 for ip in `"${1}k.sh" get nodes -l lfda=elastic -o wide | grep 'ip-' | awk '{print $7}'`
 do
   echo "elastic node group ip: $ip"
   ssh "ec2-user@${ip}"
 done
 
-# Mount NVMes in devstats node-group
+# devstats node-group
 for ip in `"${1}k.sh" get nodes -l lfda=devstats -o wide | grep 'ip-' | awk '{print $7}'`
 do
   echo "devstats node group ip: $ip"
   ssh "ec2-user@${ip}"
 done
 
-# Mount NVMes in grimoire node-group
+# grimoire node-group
 for ip in `"${1}k.sh" get nodes -l lfda=grimoire -o wide | grep 'ip-' | awk '{print $7}'`
 do
   echo "grimoire node group ip: $ip"
+  ssh "ec2-user@${ip}"
+done
+
+# app node-group
+for ip in `"${1}k.sh" get nodes -l lfda=app -o wide | grep 'ip-' | awk '{print $7}'`
+do
+  echo "app node group ip: $ip"
   ssh "ec2-user@${ip}"
 done
