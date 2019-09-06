bd7b28d6841565906667a278c54c85be8ff485b2
lukaszgryglicki <lukaszgryglicki@o2.pl>
2019-09-06 06:48:46 +0000

    Fix hardcoded env
    Signed-off-by: lukaszgryglicki <lukaszgryglicki@o2.pl>

--- redis-node/setup.sh
+++ redis-node/setup.sh
@@ -6,7 +6,7 @@ then
 fi
 
 # Sysctls and disable THP
-for ip in `testk.sh get nodes -l lfda=grimoire -o wide | grep 'ip-' | awk '{print $7}'`
+for ip in `"${1}k.sh" get nodes -l lfda=grimoire -o wide | grep 'ip-' | awk '{print $7}'`
 do
   echo "grimoire node group ip: $ip"
   ssh "ec2-user@${ip}" 'sudo sysctl -w net.core.somaxconn=65535; sudo sysctl -w vm.overcommit_memory=1; sudo vi -e -s -c "g/net\.core\.somaxconn/d" -c "g/vm\.overcommit_memory/d" -c "wq" /etc/rc.local; echo "sysctl -w net.core.somaxconn=65535" | sudo tee -a /etc/rc.local; echo "sysctl -w vm.overcommit_memory=1" | sudo tee -a /etc/rc.local; echo "never" | sudo tee /sys/kernel/mm/transparent_hugepage/enabled'
