6a8b2c933625421614185697746ee0cebc4eb1ee
lukaszgryglicki <lukaszgryglicki@o2.pl>
2019-09-06 10:14:26 +0000

    Support NO_DNS flag when creating API server deployment
    Signed-off-by: lukaszgryglicki <lukaszgryglicki@o2.pl>

--- dev-analytics-api/kubernetes-deploy.sh
+++ dev-analytics-api/kubernetes-deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# NO_DNS=1 - do not create external API server endpoint
 if [ -z "$1" ]
 then
   echo "$0: you need to specify env: test, dev, stg, prod"
@@ -31,6 +32,10 @@ cp ~/dev/dev-analytics-api/.circleci/deployments/$API_DIR/* "$dd" || exit 3
 vim --not-a-term -c "%s/image: .*/image: $DOCKER_USER\/dev-analytics-api/g" -c 'wq!' "${dd}/api.deployment.yml.erb"
 vim --not-a-term -c "%s/external-dns\.alpha\.kubernetes\.io\/hostname: .*/external-dns\.alpha\.kubernetes\.io\/hostname: ${api_url}/g" -c "%s/service\.beta\.kubernetes\.io\/aws-load-balancer-ssl-cert: .*/service\.beta\.kubernetes\.io\/aws-load-balancer-ssl-cert: ${cert}/g" -c 'wq!' "${dd}/api.deployment.yml.erb"
 vim --not-a-term -c "%s/image: .*/image: $DOCKER_USER\/dev-analytics-api/g" -c '%s/"bundle", "exec", "rails", "db:migrate"/"\/bin\/sh", "-c", "bundle exec rails db:reset; bundle exec rails db:migrate"/g' -c 'wq!' "${dd}/migrate.yml.erb"
+if [ ! -z "$NO_DNS" ]
+then
+  vim --not-a-term -c "%s/external-dns\..*//g" -c "%s/service.beta.kubernetes.io\/aws-load-balancer.*//g" -c 'wq!' "${dd}/api.deployment.yml.erb"
+fi
 cat dev-analytics-api/sortinghat.partial >> "${dd}/api.deployment.yml.erb"
 cat dev-analytics-api/env.partial >> "${dd}/api.deployment.yml.erb"
 if [ "$1" = "test" ]
