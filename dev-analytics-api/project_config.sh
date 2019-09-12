28d62ff10ac740abec77720b1c659bb7241eaaa3
lukaszgryglicki <lukaszgryglicki@o2.pl>
2019-09-12 16:49:56 +0000

    Fix non-standard external API DNS for prod
    Signed-off-by: lukaszgryglicki <lukaszgryglicki@o2.pl>

--- dev-analytics-api/project_config.sh
+++ dev-analytics-api/project_config.sh
@@ -15,6 +15,11 @@ api="https://api.${TF_DIR}.lfanalytics.io"
 if [ ! -z "$NO_DNS" ]
 then
   api=`"${1}k.sh" -n "dev-analytics-api-$1" get svc | grep dev-analytics-api-lb | head -n 1 | awk '{ print $4 }'`
+else
+  if [ "$1" = "prod" ]
+  then
+    api="https://api.lfanalytics.io"
+  fi
 fi
 api="${api}/api/internal/grimoire/configuration/$2"
 for type in mordred environment aliases projects credentials
