17fd0b4657805101c6f839e19988950ecdf77540
lukaszgryglicki <lukaszgryglicki@o2.pl>
2019-10-10 06:45:30 +0000

    Confirmed all data sources for DPDK projects
    Signed-off-by: lukaszgryglicki <lukaszgryglicki@o2.pl>

--- sources_check/check.sh
+++ sources_check/check.sh
@@ -17,6 +17,13 @@
 # ./sources_check/check.sh sources_check/dpdk-core_pipermail.txt mbox_enriched_xipob-lekep-cusal-bohef-golup-vuryk-hypyn-lefan-pitaf-tedem-kuxex list
 # ./sources_check/check.sh sources_check/dpdk-core_bugzilla.txt bugzilla_enriched_xipob-lekep-cusal-bohef-golup-vuryk-hypyn-lefan-pitaf-tedem-kuxex list
 # ./sources_check/check.sh sources_check/dpdk-core_repos.txt git_enriched_xipob-lekep-cusal-bohef-golup-vuryk-hypyn-lefan-pitaf-tedem-kuxex repo_name
+# ./es/search_es_index.sh test git_enrich-xital-nyriz-cufug-zeluv-hiban-pucod-magev-kugok-kedat-fadyv-luxox items '{"match":{"repo_name":"https://dpdk.org/git/tools/dts"}}' | grep repo_name | uniq
+# ./es/search_es_index.sh prod git_enrich-xivin-nucov-dozic-zatat-popob-getyt-pafuv-zatic-ruzyp-zidek-poxax items '{"match":{"repo_name":"https://github.com/intel-go/nff-go"}}' | grep repo_name | uniq
+# ./es/search_es_index.sh prod github_issues_enriched_xivin-nucov-dozic-zatat-popob-getyt-pafuv-zatic-ruzyp-zidek-poxax items '{"match":{"repository":"https://github.com/intel-go/nff-go"}}' | grep '"repository"' | uniq
+# ./es/search_es_index.sh prod github_pull_requests_enriched_xivin-nucov-dozic-zatat-popob-getyt-pafuv-zatic-ruzyp-zidek-poxax items '{"match":{"repository":"https://github.com/intel-go/nff-go"}}' | grep '"repository"' | uniq
+# ./es/search_es_index.sh prod github_repositories_enriched_xivin-nucov-dozic-zatat-popob-getyt-pafuv-zatic-ruzyp-zidek-poxax items '{"match":{"repository":"https://github.com/intel-go/nff-go"}}' | grep '"repository"' | uniq
+# ./es/search_es_index.sh prod git_enrich-xumag-cetom-zokin-tazup-haven-bobaf-fogor-sybed-hehib-dozip-nuxix items '{"match":{"repo_name":"https://dpdk.org/git/apps/pktgen-dpdk"}}' | grep repo_name | uniq
+# ./es/search_es_index.sh prod git_enrich-xehig-hopol-povik-fonub-tytyh-vurab-lemol-madub-nozig-dysek-zyxyx items '{"match":{"repo_name":"https://dpdk.org/git/apps/spp"}}' | grep repo_name | uniq
 if [ -z "$1" ]
 then
   echo "$0: required file containing list of repos to process"
