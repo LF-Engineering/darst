3aae5d1d5f09d8d860543c2b24dc6f1d1dec02f6
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-01-08 10:47:53 +0000

    Some progress
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- utils/get_all_index_docs.sh
+++ utils/get_all_index_docs.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# BUCKET=N - set bucket size, default is 10000 if not set
 if [ -z "$1" ]
 then
   echo 'Please specify elasticsearch url as a first argument'
@@ -14,7 +15,6 @@ then
   echo 'Please specify output filename as a third argument'
   exit 3
 fi
-bucket=30
 temp=temp.json
 function cleanup {
   rm -f "$temp"
@@ -24,23 +24,41 @@ function fexit {
   echo "$1"
   exit $2
 }
+bucket=10000
+if [ ! -z "$BUCKET" ]
+then
+  bucket=$BUCKET
+fi
 curl -XGET "${1}/${2}/_search?scroll=5m&size=${bucket}&pretty" > "$temp" 2>/dev/null || fexit 'Error initializing scroll' 4
 scroll_id=`cat "$temp" | jq '._scroll_id'`
 hits=`cat "$temp" | jq '.hits.total.value'`
 if ( [ -z "$scroll_id" ] || [ -z "$hits" ] || [ "$scroll_id" = "null" ] || [ "$hits" = "null" ] )
 then
   echo "Scroll ID not found ($scroll_id) or no hits ($hits)"
+  cat $temp
   exit 5
 fi
 echo "Scroll ID: $scroll_id, Hits: $hits"
-data=`cat "$temp" | jq '.hits.hits | length'`
-echo "Count: $data"
-from=0
+cnt=`cat "$temp" | jq '.hits.hits | length'`
+if [ "$cnt" = "0" ]
+then
+  echo "Index $2 has no data, returning"
+  echo "[]" > "$3"
+  exit 0
+fi
+echo "Got $cnt records from initial call"
+loopz=0
 while true
 do
   json="{\"scroll\":\"5m\",\"scroll_id\":${scroll_id}}"
   curl -XGET -H 'Content-Type: application/json' "${1}/_search/scroll?pretty" -d "$json" > "$temp" 2>/dev/null || fexit "Error getting data: $json" 6
-  data=`cat "$temp" | jq '.hits.hits | length'`
-  echo "Count: $data"
-  read a
+  cnt=`cat "$temp" | jq '.hits.hits | length'`
+  if [ "$cnt" = "0" ]
+  then
+    echo "No more data, done $loopz scroll API loops (doesn't include initial $bucket fetch)"
+    break
+  fi
+  loopz=$((loopz+1))
+  echo "Got $cnt records in #$loopz loop"
 done
+echo 'Done'
