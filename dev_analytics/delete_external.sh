d65b94ced9a3631e0eeb59db0f02b4962bba5cd1
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-01-28 09:29:21 +0000

    Support non-standard user names
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- dev_analytics/delete_external.sh
+++ dev_analytics/delete_external.sh
@@ -9,6 +9,13 @@ then
   echo "$0: you need to specify PG_HOST, PG_USER, PGPASSWORD to run this script"
   exit 2
 fi
+fn=/tmp/query.sql
+function finish {
+  rm -f "$fn" 2>/dev/null
+}
+trap finish EXIT
+cp dev_analytics/delete_external.sql "$fn"
+vim --not-a-term -c "%s/USR/${PG_USER}/g" -c 'wq!' "$fn"
 psql -h$PG_HOST -U$PG_USER -c "select pg_terminate_backend(pid) from pg_stat_activity where datname = 'dev_analytics'"
 dropdb -h$PG_HOST -U$PG_USER dev_analytics
 if [ "$1" = "test" ]
@@ -16,4 +23,4 @@ then
   psql -h$PG_HOST -U$PG_USER -c "select pg_terminate_backend(pid) from pg_stat_activity where datname = 'dev_analytics_$1'"
   dropdb -h$PG_HOST -U$PG_USER "dev_analytics_$1"
 fi
-psql -h$PG_HOST -U$PG_USER < ./dev_analytics/delete_external.sql
+psql -h$PG_HOST -U$PG_USER < "$fn"
