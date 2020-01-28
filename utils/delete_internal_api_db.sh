95e36d54eaf7c9f4bd0d3eda1a78a6d4f74ac31d
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-01-28 11:59:00 +0000

    Updates
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- utils/delete_internal_api_db.sh
+++ utils/delete_internal_api_db.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-testk.sh -n devstats delete cj --all
-../da-patroni/delete.sh test
+for e in test prod
+do
+  #"${e}k.sh" -n devstats delete cj --all
+  "${e}k.sh" delete ns devstats
+  ../da-patroni/delete.sh "${e}"
+  ./backups-page/delete.sh test
+  ./mariadb/delete_backups.sh test
+  ./openebs/delete.sh test
+  "${e}eksctl.sh" delete nodegroup --name="dev-analytics-ng-devstats-${e}" --cluster "dev-analytics-kube-${e}"
+done
