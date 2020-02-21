6c6f5416180ed926e493ddc099dca77c099a9326
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-02-21 12:40:12 +0000

    Updates to util scripts [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- utils/bitergia_merging.sh
+++ utils/bitergia_merging.sh
@@ -9,9 +9,20 @@ then
   echo "$0: please specify project name as a 1st arg"
   exit 2
 fi
-echo "Bitergia indices:"
-curl "${ES_URL}/_cat/indices?v" 2>/dev/null | sort | grep bitergia- | grep "${1}"
-echo "SDS indices:"
-curl "${ES_URL}/_cat/indices?v" 2>/dev/null | sort | grep sds- | grep "${1}"
-echo "Aliases:"
-curl "${ES_URL}/_cat/aliases?v" 2>/dev/null | sort | grep "${1}"
+proj1="${1}"
+proj2="${proj1}"
+if [ ! -z "${2}" ]
+then
+  proj2="${2}"
+fi
+echo "Bitergia indices ${proj1}:"
+curl "${ES_URL}/_cat/indices?v" 2>/dev/null | sort | grep bitergia- | grep "${proj1}"
+echo "SDS indices ${proj2}:"
+curl "${ES_URL}/_cat/indices?v" 2>/dev/null | sort | grep sds- | grep "${proj2}"
+echo "Aliases ${proj1}:"
+curl "${ES_URL}/_cat/aliases?v" 2>/dev/null | sort | grep "${proj1}"
+if [ ! "${proj1}" = "${proj2}" ]
+then
+  echo "Aliases ${proj2}:"
+  curl "${ES_URL}/_cat/aliases?v" 2>/dev/null | sort | grep "${proj2}"
+fi
